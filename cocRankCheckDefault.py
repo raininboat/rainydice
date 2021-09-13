04d56eb0d3a847f62c4299c70922a68fdf61f04e
raininboat <thunderain_zhou@163.com>
2021-09-13 17:19:35 +0800

    modified:   GlobalVal.py 	modified:   __init__.py 	modified:   cal_btree.py 	modified:   cocRankCheckDefault.py 	modified:   constant.py 	modified:   dice.py 	modified:   diceClass.py 	modified:   main.py

--- cocRankCheckDefault.py
+++ cocRankCheckDefault.py
@@ -5,13 +5,33 @@
  / , _/ __ |_/ //    /  \  / 
 /_/|_/_/ |_/___/_/|_/   /_/  
                              
-    COC 大成功失败判定模板（村规）
+    RainyDice 跑团投掷机器人服务 by RainyZhou
+        COC 大成功失败判定模板（村规）
 
     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     @ 注意！请勿修改本文件中的判断逻辑和内容！                  @
     @ 如需自定义村规请修改 data/rainydice/conf/rankcheck.py   @
     @ 本文件仅为生成判断文件的默认模板                         @
     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+    Copyright (C) 2021 RainyZhou
+
+    This file is part of RainyDice.
+
+    RainyDice is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    RainyDice is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with RainyDice.  If not, see <https://www.gnu.org/licenses/>
+
+    Contact Email: thunderain_zhou@163.com
 
 '''
 
--- constant.py
+++ constant.py
@@ -1,12 +1,34 @@
+# -*- coding: utf-8 -*-
 '''
-   ___  ___   _____  ____  __
-  / _ \/ _ | /  _/ |/ /\ \/ /
- / , _/ __ |_/ //    /  \  / 
-/_/|_/_/ |_/___/_/|_/   /_/  
-                             
-    COC 7th 常量
+       ___  ___   _____  ____  __
+      / _ \/ _ | /  _/ |/ /\ \/ /
+     / , _/ __ |_/ //    /  \  / 
+    /_/|_/_/ |_/___/_/|_/   /_/  
+
+    RainyDice 跑团投掷机器人服务 by RainyZhou
+        COC 7th 常量 内容部分参考 COC7th规则书 和 仑质编写的[OlivaDice]<http://oliva.dice.center/>
+
+    Copyright (C) 2021 RainyZhou
+
+    This file is part of RainyDice.
+
+    RainyDice is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    RainyDice is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with RainyDice.  If not, see <https://www.gnu.org/licenses/>
+
+    Contact Email: thunderain_zhou@163.com
 
 '''
+
 class Constant:
     def name_replace(self,skill_name):
         skill_name = str.strip(skill_name)
